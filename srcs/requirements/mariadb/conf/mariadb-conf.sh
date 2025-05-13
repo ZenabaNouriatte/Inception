07bc2a9a00b975d9354e59302f752fbb19d67a42
zmogne <zmogne@Inception.myguest.virtualbox.org>
2025-05-13 15:00:29 +0200

    vm42

--- srcs/requirements/mariadb/conf/mariadb-conf.sh
+++ srcs/requirements/mariadb/conf/mariadb-conf.sh
@@ -7,17 +7,18 @@ chown -R mysql:mysql /run/mysqld
 if [ ! -d "/var/lib/mysql/mysql" ]; then
     mysql_install_db --user=mysql --datadir=/var/lib/mysql > /dev/null
 
-    TEMP_SQL=$(mktemp)
-    cat << EOF > "$TEMP_SQL"
+cat << EOF > /tmp/init.sql
 ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';
 CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;
+DROP USER IF EXISTS '${DB_USER}'@'%';
 CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
 GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO '${DB_USER}'@'%';
 FLUSH PRIVILEGES;
 EOF
 
-    mysqld --user=mysql --bootstrap --datadir=/var/lib/mysql < "$TEMP_SQL"
-    rm -f "$TEMP_SQL"
+    mysqld --user=mysql --bootstrap --datadir=/var/lib/mysql < /tmp/init.sql
+
+    rm -f /tmp/init.sql
 fi
 
 exec mysqld_safe --datadir=/var/lib/mysql --socket=/run/mysqld/mysqld.sock --bind-address=0.0.0.0
